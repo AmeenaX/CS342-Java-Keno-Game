2fb520e2c12ed8876fa06b39bb0e52c69805a299
Ameena <159801402+AmeenaX@users.noreply.github.com>
2025-10-31 18:02:48 -0500

    Create Project2Fall2025.java

---
+++ Project2Fall2025.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Project2Fall2025 extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("Keno");
+
+	     
+		Welcome welcome = new Welcome();
+		primaryStage.setScene(welcome.WelcomeScene(primaryStage));
+		primaryStage.show();
+		
+	}
+
+}
